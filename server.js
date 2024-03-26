75cd09365434f3c6bc84de67ed174fab11396673
Yatin Jindal <jindalyatin2109@gmail.com>
2024-03-26 23:32:14 +0530

    added

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var fileuploader = require("express-fileupload");
+var mysql = require("mysql2");
+
+var app = express();
+
+app.listen(2004, function () {
+   console.log("server started");
+})
+
+app.use(express.static("public"));
+app.use(fileuploader());
+
+app.get("/", function (req, resp) {
+   resp.sendFile(process.cwd() + "/public/index.html");
+})
+
