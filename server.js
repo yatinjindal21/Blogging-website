5e3d37dbe9cf7982d290f4312c72719a1350e83c
Yatin Jindal <jindalyatin2109@gmail.com>
2024-08-17 10:30:15 +0530

    changes in iframe

--- server.js
+++ server.js
@@ -206,6 +206,13 @@ app.get("/get-publisher", function (req, resp) {
    })
 })
 
+
+//-------------------------- SHOW ALL BLOGS =================================
+
+app.get("/show-all-blogs", function (req,res){
+   res.render('blog/index');
+})
+
 //-------------------------- GET ALL BLOGS ===============================
 
 app.get("/get-all-blogs", function (req, resp) {
