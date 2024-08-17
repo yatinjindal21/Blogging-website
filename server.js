1cb5a7700881f2380fe988cf817fe592663e58ae
simpstr <ketakialoni@gmail.com>
2024-08-17 10:33:56 +0530

    commit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var fileuploader = require("express-fileupload");
 var mysql = require("mysql2");
 const path=require('path');
 const ejsMate=require('ejs-mate');
+const bodyParser = require('body-parser');
+
+
 
 const dbCon = require('./config/db');
 
@@ -23,7 +26,7 @@ app.listen(2004, function () {
 
 app.use(express.static("public"));
 app.use(fileuploader());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded(true));
 
 
@@ -330,6 +333,12 @@ app.get("/open-searched-one", function (req, resp) {
       }
       else {
          resp.send(err);
-      }
+      }t
    })
+})
+
+
+app.post('/view-blog/:id/comment',(req,res)=>{
+   const body=req.body.comment;
+   
 })
